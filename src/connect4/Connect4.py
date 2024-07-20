982f2391d4dd47d581b417f846f0f6e36bd4b54f
kilianvincent <kilianvincen@gmail.com>
2024-07-20 12:39:52 +0200

    Connect4 structure description

---
+++ src/connect4/Connect4.py
@@ -0,0 +1,15 @@
+class Connect4:
+    def __init__(self):
+        self._grid = None
+
+    def play(self, column: int) -> bool:
+        return False
+
+    def print(self):
+        return
+
+    def get_grid(self):
+        return self._grid
+
+    def is_finished(self):
+        return False
