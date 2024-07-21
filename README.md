e93001398e34fc8a0086001e475768191e065eab
kilianvincent <kilianvincen@gmail.com>
2024-07-21 16:15:57 +0200

    Automatic update of the README.md with the coverage value

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # <span style="color:purple"> **Connect 4 AI** </span>
 
 <span style="color:purple">*Genetic AI*</span> model that plays Connect 4.
+
+`Coverage: 100%`
--- test.sh
+++ test.sh
@@ -1,4 +1,11 @@
 pipenv run coverage run -m unittest test/test_Connect4.py
 
 pipenv run coverage html
+
+cov=$(pipenv run coverage report)
+cov_val="${cov: -4}"
+readme_file="README.md"
+pattern="Coverage: "
+sed -i "/$pattern/s/\($pattern\).\{4\}/\1$cov_val/" "$readme_file"
+
 read -p "Press any key to continue" x
