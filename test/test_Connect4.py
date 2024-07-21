1f926e5f4c02f43b8110670adace3eac7dc747db
kilianvincent <kilianvincen@gmail.com>
2024-07-21 11:34:22 +0200

    Update of tests of Connect4 class

--- test/test_Connect4.py
+++ test/test_Connect4.py
@@ -9,18 +9,21 @@ class TestConnect4(unittest.TestCase):
 
     def test_play(self):
         self._ref_array[0][0] = 1
-        self._connect4.play(0)
+        self._connect4.play(0, 1)
         self.assertEqual(self._ref_array, self._connect4.get_grid())
-        self.assertRaises(ValueError, self._connect4.play, -1)
-        self.assertRaises(ValueError, self._connect4.play, 7)
+        self.assertRaises(ValueError, self._connect4.play, -1, 1)
+        self.assertRaises(ValueError, self._connect4.play, 7, 1)
         for _ in range(6):
-            self._connect4.play(2)
-        self.assertRaises(ValueError, self._connect4.play, 2)
+            self._connect4.play(2, 1)
+        self.assertRaises(Exception, self._connect4.play, 2, 1)
+        self.assertRaises(ValueError, self._connect4.play, 4, 8)
+        self.assertRaises(ValueError, self._connect4.play, 4, 0)
+        self.assertRaises(ValueError, self._connect4.play, 4, -1)
 
     def test_str(self):
         self.assertEqual(str(self._connect4), (('0'*7) + '\n')*6)
-        self._connect4.play(1)
-        self.assertEqual(str(self._connect4), (('0'*7)+'\n')*5 + '0100000')
+        self._connect4.play(1, 1)
+        self.assertEqual(str(self._connect4), (('0'*7)+'\n')*5 + '0100000\n')
 
     def test_is_finished(self):
         self.assertEqual(self._connect4.is_finished(), False)
